1eac8551fead37c30e6c9f8fef1fa1f5cf283e88
MAZEAU Antoine <antoine.mazeau31@gmail.com>
2023-01-04 18:50:17 +0100

    Connexion à un compte

--- Applications/Web/CreerCompte/ConnexionCompte.php
+++ Applications/Web/CreerCompte/ConnexionCompte.php
@@ -16,11 +16,7 @@
     </head>
 
 <body>
-<?php
-    if(isset($_POST['valider'])){
-        header('location:MonCompte.php');
-    }
-?>
+
 
 <div class="background-image"></div>
 <div class="content">
@@ -28,17 +24,37 @@
 <!--  <form action="TraitConnexion.php" method="POST">  
     avec la base de donné on pourras l'utiliser -->
 
-<form class="formulaire" method="post">
-    <label for="login">Login:</label>
+    <form class="formulaire" method="post">
+    <label for="login">Adresse mail :</label>
     <input type="text" id="login" name="login" required>
     <label for="mdp">Mot de passe:</label>
     <input type="password" id="mdp" name="mdp" required>
     <input type='submit' name='valider' value='valider' />
     <div class="creercompte">
-        <a href="CreerCompte.php" ><p>Créer un compte </p></a>
+            <a href="CreerCompte.php" ><p>Créer un compte </p></a><br><br><br>
+<?php
+    session_start();
+    if(isset($_POST['valider'])){
+        $req = "SELECT * FROM Client";
+        $lesComptes = oci_parse($connect, $req);
+        $result = oci_execute($lesComptes);
+        if (!$result) {
+            $e = oci_error($lesComptes);
+            print htmlentities($e['message'].' pour cette requete : '.$e['sqltext']);		
+        }
+        while (($unCompte = oci_fetch_assoc($lesComptes)) != false) {
+            if($unCompte['EMAILC'] == htmlentities($_POST["login"]) && password_verify(htmlentities($_POST["mdp"]),$unCompte['MDPC']) ) {
+                $_SESSION['acces'] = "OK";
+				$_SESSION['nom'] = $unCompte['PRENOMC'];
+                header('location:MonCompte.php');
+            }
+        }
+        echo "Adresse mail / mot de passe incorrect ... ";
+    }
+?>
+    
     </div>
-
-  </form>
+</form>
 </div>
 
 </body>     
