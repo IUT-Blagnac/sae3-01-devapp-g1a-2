495b937998c401308db7f94c5855d63601d8b74a
AdrielMarchant <101397536+AdrielMarchant@users.noreply.github.com>
2023-01-08 16:11:58 +0100

    Update mise_a_jour_BD.sql
    ajout de la table pannier

--- BD/mise_a_jour_BD.sql
+++ BD/mise_a_jour_BD.sql
@@ -15,3 +15,15 @@ ADD CONSTRAINT ck_Article_stock CHECK (stockA >= 0);
 
 ALTER TABLE Article
 ADD CONSTRAINT ck_Article_nbEtoiles CHECK ( nbEtoilesA >= 0 AND nbEtoilesA <= 5);
+
+DROP TABLE Pannier;
+CREATE TABLE Pannier
+(
+    numC NUMBER,
+    refA VARCHAR(30),
+    quantite NUMBER,
+    CONSTRAINT pk_Pannier PRIMARY KEY (numC,refA),
+    CONSTRAINT fk_Pannier_numC FOREIGN KEY (numC) REFERENCES Client(numC),
+    CONSTRAINT fk_Pannier_refA FOREIGN KEY (refA) REFERENCES Article(refA),
+    CONSTRAINT ck_Pannier_quantite CHECK (quantite >0)
+);
